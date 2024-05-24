8fb58eb32cfad958f12544583be9a7f4b8bcf6b9
avillaq <villafuertequispealex@gmail.com>
2024-05-23 22:39:29 -0500

    Copiamos el codigo anterior y ahora colocamos el casillero negro primero

--- Ejercicio2e.py
+++ Ejercicio2e.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+casillero1 = square.negative()
+casillero2 = square
+
+union = casillero1.join(casillero2)
+
+draw(union.horizontalRepeat(3))
