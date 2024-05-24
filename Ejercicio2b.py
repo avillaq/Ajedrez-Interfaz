eab964a3b430017fc908f9955aaa005b74505052
avillaq <villafuertequispealex@gmail.com>
2024-05-23 22:25:41 -0500

    Para la imagen b, hacemos una copia del codigo usado en la imagen a y simplemente realizamos cambios en las piezas inferiores añadiendoles la funcion verticalMirror

--- Ejercicio2b.py
+++ Ejercicio2b.py
@@ -1,2 +1,7 @@
 from interpreter import draw
 from chessPictures import *
+
+col1 = knight.under(knight.negative().verticalMirror())
+col2 = knight.negative().under(knight.verticalMirror())
+
+draw(col1.join(col2))
