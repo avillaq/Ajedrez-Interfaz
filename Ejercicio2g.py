06a2d739b38be24ad809e2a1626289280d7ad20f
avillaq <villafuertequispealex@gmail.com>
2024-05-24 09:49:39 -0500

    PrimEro se crearan los piezas negras. Empezaremos con las torres

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -1,10 +1,12 @@
 from interpreter import draw
 from chessPictures import *
 
-fila1 = square.join(square.negative()).horizontalRepeat(3)
-fila2 = fila1.negative()
+### Piezas Negras
 
-union = fila1.under(fila2)
+# Torres
+torreIzquierdo = rock.negative().ponerEnCasillero(0)
+torreDerecho = rock.negative().ponerEnCasillero(1)
 
-draw(union.ponerEnCasillero(1))
+
+draw(torreDerecho)
 
