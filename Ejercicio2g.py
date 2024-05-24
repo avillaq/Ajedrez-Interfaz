ac7ac02b5d621c2adfc3ccf786c6e97352073f01
avillaq <villafuertequispealex@gmail.com>
2024-05-24 09:53:07 -0500

    Para los caballos es necesario cambiar el orden del color de los casilleros un poco

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -7,6 +7,11 @@ from chessPictures import *
 torreIzquierdo = rock.negative().ponerEnCasillero(0)
 torreDerecho = rock.negative().ponerEnCasillero(1)
 
+# Caballo
+caballoIzquierdo = knight.negative().ponerEnCasillero(1)
+caballoDerecho = knight.negative().ponerEnCasillero(0)
 
-draw(torreDerecho)
+
+
+draw(caballoIzquierdo)
 
